1f4e8611550098f08beb1dc542ea4124cb72f17d
JQueen <queen_jenf@yahoo.com>
2020-08-12 19:02:24 -0700

    files from 4.2.7 data obj

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # task_module
 Kanban Board
+notes for ensuring you split the code:
+
+//One to handle the form submission, get the form values, and pass those values to another function as arguments
+//One to accept the form values as arguments and use them to create the new task item's HTML
+
+Let's take a moment and outline what we'll do to refactor the code:
+Rename the handler function to be a little more specific to the event it's handling.
+
+Create a new function to take in the task's name and title as arguments and create the HTML elements that get added to the page.
+
+Move the code that creates and adds HTML elements from the handler function into the newly created function.
+
+Update the handler function to send the task name and type values from the form to the newly created function.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,42 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function(event) {
   event.preventDefault();
-  
-  //listItemEl.textContent = taskNameInput = "This is a new task.";
-  //var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  // package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+}
 
 // create list item
 var listItemEl = document.createElement("li");
 listItemEl.className = "task-item";
 
+
 // create div to hold task info and add to list item
 var taskInfoEl = document.createElement("div");
-// give it a class name
 taskInfoEl.className = "task-info";
-// add HTML content to div
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+taskInfoEl.innerHTML = taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
 listItemEl.appendChild(taskInfoEl);
 
 // add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
 
-
-//One to handle the form submission, get the form values, and pass those values to another function as arguments
-//One to accept the form values as arguments and use them to create the new task item's HTML
+// taskDataObj
+{
+  //name: "Task's name",
+  //type: "Task's type"
 }
+  
 
-buttonEl.addEventListener("click", createTaskHandler);
+buttonEl.addEventListener("click", taskFormHandler);
 
